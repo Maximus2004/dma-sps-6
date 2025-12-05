d48a569142f0c6c80d89859986051a8db3e3c3d5
Dmitriev Maxim <maks09062004@mail.ru>
2025-12-05 14:30:12 +0300

    change math logic

--- hypotenuse.py
+++ hypotenuse.py
@@ -4,6 +4,8 @@ def get_hypotenuse(a, b):
     return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 
 if __name__ == "__main__":
-    a = 1
-    b = 2
-    print(get_hypotenuse(a,b))
+    print("Введи а:")
+    a = int(input())
+    print("Введи b:")
+    b = int(input())
+    print("c =", get_hypotenuse(a,b))
